d5db24c8f04a3e889c305f3bb8d0c3df3f5e9467
Devraj Pujari <devrajpujari292@gmail.com>
2025-09-21 02:28:58 +0530

    UPDATE package.json for deployement

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use((err, req, res, next) => {
   res.status(status).send({ success: false, message });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
