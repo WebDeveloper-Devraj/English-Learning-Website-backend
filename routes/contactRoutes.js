a91cdc794af5d0a08782e913aa589baa732809ac
Devraj Pujari <devrajpujari292@gmail.com>
2025-09-21 02:58:33 +0530

    UPDATE contactRoutes.js

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const router = express.Router();
-const { userVerification } = require("../middlewares/AuthMiddleware");
+const { userVerification } = require("../middlewares/AuthMiddleware.js");
 const { sendMail } = require("../controllers/contactController");
 
 router.post("/", userVerification, sendMail);
